07916fe39fbd88d0ddcf185b9675b0d6a52e81c3
RaisinBrand <brandansb@ucla.edu>
2025-05-05 15:03:20 -0700

    more login page stuff

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        {/* Redirect "/" to "/login" */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Signup Page */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
